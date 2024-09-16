b13b48cc68bd6e2a6f982be4a93325e818e30505
ANAMASGARD <chaudharygaurav2004@gmail.com>
2024-09-16 22:04:08 +0530

    Rotation at initial stage has been added into our island

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 import planeScene from "../assets/3d/plane.glb";
 import { useGLTF } from '@react-three/drei';
 
-const Plane = () => {
+const Plane = ({isRotating, ...props}) => {
     const { scene ,aniamtions } = useGLTF(planeScene);
   return (
-    <mesh>
+    <mesh {...props}>
         <primitive object={scene} /> 
     </mesh>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/no-unknown-property */
-import {Suspense } from 'react'
+import {useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
-import Island from '../models/island';
+import Island from '../models/Island';
 import  Sky  from '../models/Sky';
 import  Bird from '../models/Bird';
 import  Plane  from '../models/Plane';
 
 const Home = () => {
+const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale= null;
     let screenPosition = [0,-6.5,-43];
@@ -23,16 +25,35 @@ const Home = () => {
 
     return [screenScale, screenPosition, rotation  ];
   };
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
 
-  // const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
 
   return (
     <section className ="w-full h-screen relativee">
-      <Canvas className="w-full h-screen relative"
-      camera = {{ near : 0.1, far:1000 }}
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
       >
+      
         <Suspense fallback={<Loader />}> 
             <directionalLight position ={[1,1,1]} intensity={2} />
             <ambientLight intensity={0.5}/>
@@ -47,8 +68,15 @@ const Home = () => {
             <Island 
             position={islandPosition}
             scale={islandScale}
-            rotation={islandRotation} />
-            <Plane />
+            rotation={islandRotation} 
+            isRotating={isRotating}
+            setIsRotating = { setIsRotating}
+            />
+            <Plane
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0,20,0]} />
         </Suspense>
 
 
