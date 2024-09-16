988b7c31ed4d717c5265c095044948c6bbde278f
ANAMASGARD <chaudharygaurav2004@gmail.com>
2024-09-16 15:48:00 +0530

    necessary changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import{ Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+// import { Home, Leaderboard, Play, Login } from './pages';
+// import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div> <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  </div>
+    <main className ="bg-slate-300/20" >
+      <Router>
+        {/* <Navbar /> */}
+        Navabar
+        <Routes>
+          <Route path="/" element={'Home' } />
+          {/* <Route path="/leaderboard" element={<Leaderboard /> } />
+          <Route path="/play" element={<Play /> } />
+          <Route path="/login" element={<Login /> } /> */}
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="header">
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center 
+      justify-center flex font-bold shadow-md" >
+        <p className="blue-gradient_text"> ZEDO </p>
+      </NavLink>
+    </header>
   )
 }
 
